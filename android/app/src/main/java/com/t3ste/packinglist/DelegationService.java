b0f51e62d9945008b148c3ad606556a404dadd77
t3ste <t3ste@users.noreply.github.com>
2026-09-04 14:51:41 +0200

    Add Android app (Trusted Web Activity wrapper via Bubblewrap)

---
+++ android/app/src/main/java/com/t3ste/packinglist/DelegationService.java
@@ -0,0 +1,14 @@
+package com.t3ste.packinglist;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
